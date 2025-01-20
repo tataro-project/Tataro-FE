cc0ee4a1dfaf66e7e9c282f43ff979f6589f77c4
Seyoon Jeong <seyoonagain@gmail.com>
2025-01-21 02:52:22 +0900

    feat: layout.tsx에 LayerPopup 컴포넌트 추가

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next';
 import '@/styles/globals.css';
 import Providers from '@/components/providers/Providers';
+import LayerPopup from '@/components/common/layerPopup/LayerPopup';
 
 export const metadata: Metadata = {
   title: '하쿠나마타타로',
@@ -21,6 +22,7 @@ const RootLayout = ({
       <body>
         <Providers>
           <main>{children}</main>
+          <LayerPopup />
         </Providers>
       </body>
     </html>
