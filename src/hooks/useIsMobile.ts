c68bee812e22de92cf5396f84103251bf751b48a
Seyoon Jeong <seyoonagain@gmail.com>
2025-01-22 11:29:26 +0900

    feat: 화면 크기가 모바일인지 아닌지 구분하는 useIsMobile 훅 생성

---
+++ src/hooks/useIsMobile.ts
@@ -0,0 +1,21 @@
+import { useState, useEffect } from 'react';
+
+const useIsMobile = (breakpoint: number = 768): boolean | null => {
+  const [isMobile, setIsMobile] = useState<boolean | null>(null);
+
+  useEffect(() => {
+    const mediaQuery = window.matchMedia(`(max-width: ${breakpoint}px)`);
+
+    const handleChange = (e: MediaQueryListEvent | MediaQueryList) => {
+      setIsMobile(e.matches);
+    };
+
+    handleChange(mediaQuery);
+
+    mediaQuery.addEventListener('change', handleChange);
+    return () => mediaQuery.removeEventListener('change', handleChange);
+  }, [breakpoint]);
+  return isMobile;
+};
+
+export default useIsMobile;
