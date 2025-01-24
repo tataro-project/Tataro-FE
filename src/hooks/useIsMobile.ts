977c971687be00ced88062a3aa65999b81209add
Seyoon Jeong <seyoonagain@gmail.com>
2025-01-24 14:45:35 +0900

    feat: useIsMobile에 isMobileDevice 변수 추가

--- src/hooks/useIsMobile.ts
+++ src/hooks/useIsMobile.ts
@@ -1,7 +1,10 @@
 import { useState, useEffect } from 'react';
 
-const useIsMobile = (breakpoint: number = 768): boolean | null => {
+const useIsMobile = (
+  breakpoint: number = 768,
+): { isMobile: boolean | null; isMobileDevice: boolean } => {
   const [isMobile, setIsMobile] = useState<boolean | null>(null);
+  const isMobileDevice = /Mobi|Android|iPhone|iPad|iPod/i.test(navigator.userAgent);
 
   useEffect(() => {
     const mediaQuery = window.matchMedia(`(max-width: ${breakpoint}px)`);
@@ -17,7 +20,7 @@ const useIsMobile = (breakpoint: number = 768): boolean | null => {
     return () => mediaQuery.removeEventListener('change', handleChange);
   }, [breakpoint]);
 
-  return isMobile;
+  return { isMobile, isMobileDevice };
 };
 
 export default useIsMobile;
