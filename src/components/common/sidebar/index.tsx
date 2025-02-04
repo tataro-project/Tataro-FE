7eee8287f259362d6af11b08bd58c999f8fe1f44
Seyoon Jeong <seyoonagain@gmail.com>
2025-02-04 13:11:34 +0900

    feat: 사이드바 레이아웃 구현

---
+++ src/components/common/sidebar/index.tsx
@@ -0,0 +1,67 @@
+import Button from '@common/button';
+import clsx from 'clsx';
+import { X } from 'lucide-react';
+import SIDEBAR_MENUS from './constants';
+import { useRouter } from 'next/navigation';
+import { FocusTrap } from 'focus-trap-react';
+import useOutsideClick from '@/hooks/useOutsideClick';
+import SidebarProps from './types';
+
+const Sidebar = ({ isOpen, close }: SidebarProps) => {
+  const ref = useOutsideClick(() => {
+    if (isOpen) close();
+  });
+  const router = useRouter();
+
+  const handleMenuClick = (path: string) => {
+    router.push(path);
+    close();
+  };
+
+  return (
+    <FocusTrap active={isOpen} focusTrapOptions={{ initialFocus: false }}>
+      <div className="fixed">
+        <div
+          className={clsx(
+            'fixed top-0 left-0 w-screen h-screen bg-purple bg-opacity-10 pointer-events-none transition duration-300',
+            !isOpen && 'opacity-0',
+          )}
+        />
+
+        <div
+          ref={ref}
+          className={clsx(
+            'fixed top-0 right-0 z-40 w-60 md:w-64 h-full px-6 pt-12 pb-6 border-l border-purple bg-softPink transition duration-300',
+            isOpen && 'opacity-100 -translate-x-0',
+            !isOpen && 'opacity-0 translate-x-72 md:translate-x-80',
+          )}
+        >
+          <button onClick={close} className="absolute top-1 left-1">
+            <X strokeWidth={1.5} className="text-purple" />
+          </button>
+
+          <div className="flex flex-col justify-between items-start h-full">
+            <div className="flex flex-col gap-10 relative right-14">
+              <Button variant="menuButton" disabled>
+                Menu
+              </Button>
+              <div className="flex flex-col gap-5 grow">
+                {SIDEBAR_MENUS.map(({ menu, path }) => (
+                  <Button key={menu} variant="menuButton" onClick={() => handleMenuClick(path)}>
+                    {menu}
+                  </Button>
+                ))}
+              </div>
+            </div>
+
+            <Button variant="simple" className="self-end font-lilita text-lg md:text-xl">
+              Logout
+            </Button>
+          </div>
+        </div>
+      </div>
+    </FocusTrap>
+  );
+};
+
+export default Sidebar;
