4dcd5788ef2c9b860d1438c0a3788700c5a020aa
Seyoon Jeong <seyoonagain@gmail.com>
2025-02-07 21:16:30 +0900

    design: inputWrapper에 shrink-0 적용

--- src/components/common/inputs/inputWrapper.tsx
+++ src/components/common/inputs/inputWrapper.tsx
@@ -15,7 +15,7 @@ const InputWrapper = ({ id, label, children, error }: InputWrapperProps) => {
   if (isMobile === null) return null;
 
   return (
-    <div className="flex flex-col sm:flex-row justify-center items-start gap-1 sm:gap-6 w-full sm:h-16">
+    <div className="flex flex-col sm:flex-row justify-center items-start gap-1 sm:gap-6 shrink-0 w-full sm:h-16">
       <div className="flex justify-end items-center gap-4 max-sm:flex-1 sm:w-28">
         {label && (
           <label htmlFor={id} className={twMerge(labelStyles({ isMobile }))}>
