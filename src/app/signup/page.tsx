b74384bb521ee1359f185953796772ab83a3e831
Seyoon Jeong <seyoonagain@gmail.com>
2025-02-07 21:19:06 +0900

    feat: 프로필 폼을 비즈니스 프로필 폼의 비즈니스 로직과 프레젠테이션 로직 분리

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import ProfileForm from '@/components/myPage/profile/ProfileForm';
+import ProfileFormContainer from '@/components/myPage/profile/ProfileFormContainer';
 import ContentBox from '@common/contentBox';
 
 const SignUp = () => {
@@ -9,7 +9,7 @@ const SignUp = () => {
       <ContentBox size="max-w-2xl max-h-[768px]" layout="p-4 md:p-6">
         <section className="flex flex-col items-center gap-4 w-full h-full">
           <h3 className="font-lilita text-cream stroke text-3xl md:text-4xl">Sign Up</h3>
-          <ProfileForm />
+          <ProfileFormContainer />
         </section>
       </ContentBox>
     </>
