4dedc7b52dd6968bf99bae51dc30e049334d16c6
Sangwoo Shin <ssangwoo96@gmail.com>
2025-02-10 16:52:54 +0900

    feat: 메인페이지 애니메이션 구현 완료

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -32,21 +32,27 @@ const Home = () => {
       <div className="md:max-w-4xl max-w-md">
         <TaroSwiper />
       </div>
-      <Image
-        src={blueCloud}
-        alt="blue-cloud"
-        className="absolute left-0 md:translate-y-[400px] translate-y-[1200px] z-20 md:w-[700px] w-[300px]"
-        data-aos="fade-up"
-        data-aos-easing="200"
-      />
+      <div className="absolute left-0 md:translate-y-[400px] translate-y-[2000px] z-20 md:w-[700px] w-[300px]">
+        <Image
+          src={blueCloud}
+          alt="blue-cloud"
+          data-aos="fade-right"
+          data-aos-easing="400"
+          data-aos-offset="950"
+          data-aos-duration="1000"
+        />
+      </div>
       <Link href={'tarotReading'} className="z-50">
         <Button variant="startButton" className="md:text-2xl text-xl ">
           상담 시작 하기
         </Button>
       </Link>
       <div
-        className="flex flex-col h-full p-20 text-center text-purple font-gMedium delay-[300ms] duration-[600ms] taos:translate-y-[100%] taos:opacity-0"
-        data-taos-offset="300"
+        className="flex flex-col h-full p-20 text-center text-purple font-gMedium"
+        data-aos="fade-up"
+        data-aos-easing="200"
+        data-aos-offset="300"
+        data-aos-duration="1000"
       >
         <p>운명적인 사랑을 찾는 첫걸음, 당신만의 타로 이야기</p>
         <br />
@@ -59,15 +65,23 @@ const Home = () => {
         <br />
         <p>하쿠나마타타로</p>
       </div>
-      <Image
-        src={pinkCloud}
-        alt="pink-cloud"
-        width={600}
-        className="absolute right-0 translate-y-[1100px] md:w-[700px] w-[300px] "
+      <div className="absolute right-0 translate-y-[1100px] md:w-[600px] w-[300px] z-10">
+        <Image
+          src={pinkCloud}
+          alt="pink-cloud"
+          priority
+          data-aos="fade-up"
+          data-aos-offset="1400"
+          data-aos-duration="1000"
+        />
+      </div>
+      <div
+        className="grid grid-cols-2 gap-4 p-10"
         data-aos="fade-up"
-        data-aos-easing="200"
-      />
-      <div className="grid grid-cols-2 gap-4 mt-96 p-10" data-aos="fade-up" data-aos-easing="200">
+        data-aos-easing="400"
+        data-aos-offset="250"
+        data-aos-duration="1000"
+      >
         {bestReviews.map(review => (
           <ReviewBox
             key={review.id}
