4c48a0683e89923d06256f276f079051bee78ccc
Seyoon Jeong <seyoonagain@gmail.com>
2025-02-13 04:57:19 +0900

    fix: 클라이언트 측에서만 sessionStorage에 접근할 수 있도록 코드 수정

--- src/stores/userStore.ts
+++ src/stores/userStore.ts
@@ -11,8 +11,12 @@ type UserState = {
 };
 
 const useUserStore = create<UserState>(set => {
-  const userFromSessionStorage = sessionStorage.getItem('user');
-  const initialUser = userFromSessionStorage ? JSON.parse(userFromSessionStorage) : null;
+  let initialUser = null;
+
+  if (typeof window !== 'undefined') {
+    const userFromSessionStorage = sessionStorage.getItem('user');
+    initialUser = userFromSessionStorage ? JSON.parse(userFromSessionStorage) : null;
+  }
 
   // 창 닫기 및 탭 닫기 시 accessToken 초기화 로직 구현 필요
 
@@ -22,26 +26,30 @@ const useUserStore = create<UserState>(set => {
     setUser: ({ user, accessToken }) => {
       set({ user });
 
-      sessionStorage.setItem('user', JSON.stringify(user));
+      if (typeof window !== 'undefined') {
+        sessionStorage.setItem('user', JSON.stringify(user));
 
-      if (accessToken) {
-        const expirationTime = 1000 * 60 * 60;
+        if (accessToken) {
+          const expirationTime = 1000 * 60 * 60;
 
-        nookies.set(null, 'accessToken', accessToken, {
-          maxAge: expirationTime / 1000,
-          path: '/',
-          secure: true,
-          sameSite: 'Lax',
-        });
-        setTimeout(() => sessionStorage.removeItem('user'), expirationTime);
+          nookies.set(null, 'accessToken', accessToken, {
+            maxAge: expirationTime / 1000,
+            path: '/',
+            secure: true,
+            sameSite: 'Lax',
+          });
+          setTimeout(() => sessionStorage.removeItem('user'), expirationTime);
+        }
       }
     },
 
     resetUser: () => {
       set({ user: null });
 
-      nookies.destroy(null, 'accessToken');
-      sessionStorage.removeItem('user');
+      if (typeof window !== 'undefined') {
+        nookies.destroy(null, 'accessToken');
+        sessionStorage.removeItem('user');
+      }
     },
   };
 });
