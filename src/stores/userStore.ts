83043660820d6354fce027a782415f2ed849e717
Seyoon Jeong <seyoonagain@gmail.com>
2025-02-13 04:36:07 +0900

    feat: 회원 정보 상태 관리 userStore 생성

---
+++ src/stores/userStore.ts
@@ -0,0 +1,49 @@
+import nookies from 'nookies';
+import { create } from 'zustand';
+
+import { ProfileFormType as UserData } from '@/components/myPage/profile/types';
+
+type UserState = {
+  user: UserData | null;
+
+  setUser: (loginResponse: { user: UserData; accessToken?: string }) => void;
+  resetUser: () => void;
+};
+
+const useUserStore = create<UserState>(set => {
+  const userFromSessionStorage = sessionStorage.getItem('user');
+  const initialUser = userFromSessionStorage ? JSON.parse(userFromSessionStorage) : null;
+
+  // 창 닫기 및 탭 닫기 시 accessToken 초기화 로직 구현 필요
+
+  return {
+    user: initialUser,
+
+    setUser: ({ user, accessToken }) => {
+      set({ user });
+
+      sessionStorage.setItem('user', JSON.stringify(user));
+
+      if (accessToken) {
+        const expirationTime = 1000 * 60 * 60;
+
+        nookies.set(null, 'accessToken', accessToken, {
+          maxAge: expirationTime / 1000,
+          path: '/',
+          secure: true,
+          sameSite: 'Lax',
+        });
+        setTimeout(() => sessionStorage.removeItem('user'), expirationTime);
+      }
+    },
+
+    resetUser: () => {
+      set({ user: null });
+
+      nookies.destroy(null, 'accessToken');
+      sessionStorage.removeItem('user');
+    },
+  };
+});
+
+export default useUserStore;
