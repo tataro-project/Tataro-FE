e604811d90e1a5fa84f6fea7ae307a45c3988293
Seyoon Jeong <seyoonagain@gmail.com>
2025-02-13 13:04:09 +0900

    feat: 네이버 로그인 로직 구현

---
+++ src/app/login/naver/callback/page.tsx
@@ -0,0 +1,32 @@
+'use client';
+
+import { Suspense, useEffect } from 'react';
+import { useSearchParams } from 'next/navigation';
+
+import useUserActions from '@/hooks/useUserActions';
+
+import LoadingSpinner from '@common/loadingSpinner';
+
+const NaverCallback = () => {
+  const searchParams = useSearchParams();
+  const code = searchParams.get('code');
+  const { login } = useUserActions();
+
+  useEffect(() => {
+    if (!code) return;
+
+    login({ provider: 'naver', code });
+  }, [code, login]);
+
+  return <></>;
+};
+
+const NaverCallbackWithSuspense = () => {
+  return (
+    <Suspense fallback={<LoadingSpinner />}>
+      <NaverCallback />
+    </Suspense>
+  );
+};
+
+export default NaverCallbackWithSuspense;
