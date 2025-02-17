bfd22717a47b1633e324c2ca031bf26abbbbea98
Kwonyeojiny <kangagi0009@naver.com>
2025-02-17 18:46:18 +0900

    feat: 타로 상담 결과 버블 표시 및 팝업 모달 기능 추가

--- src/components/common/chatBubble/index.tsx
+++ src/components/common/chatBubble/index.tsx
@@ -1,10 +1,16 @@
+import Image from 'next/image';
 import { twMerge } from 'tailwind-merge';
 
+import TarotChatDetail from '@/components/tarotReading/tarotChatDetail';
 import useScreenWidth from '@/hooks/useScreenWidth';
+
+import { layerCard } from '@common/layerCard';
+
+import CardBack from '@images/CardBack.svg';
 
 import { ChatBubbleProps } from './types';
 
-const ChatBubble: React.FC<ChatBubbleProps> = ({ message, isChatbot }) => {
+const ChatBubble: React.FC<ChatBubbleProps> = ({ message, isChatbot, tarotCard }) => {
   const { isMobile } = useScreenWidth();
 
   return (
@@ -17,7 +23,36 @@ const ChatBubble: React.FC<ChatBubbleProps> = ({ message, isChatbot }) => {
           isMobile ? 'max-w-72 text-sm' : 'max-w-80 text-base',
         )}
       >
-        {message}
+        {tarotCard ? (
+          <div
+            className="flex items-start gap-4"
+            onClick={() => {
+              layerCard({
+                content: <TarotChatDetail />,
+                size: 'max-w-[680px] max-h-[800px]',
+              });
+            }}
+          >
+            <div className="relative w-24 h-40 rounded-lg flex-shrink-0 select-none">
+              <Image
+                src={tarotCard.url || CardBack}
+                alt="tarotCard"
+                layout="fill"
+                objectFit="cover"
+                draggable="false"
+                style={{
+                  transform: tarotCard?.direction === '역방향' ? 'rotate(180deg)' : 'none',
+                }}
+              />
+            </div>
+            <div className="flex flex-col justify-start items-center gap-12">
+              <div className="pt-4">{tarotCard.name} 카드</div>
+              <div className="text-xs select-none">{message}</div>
+            </div>
+          </div>
+        ) : (
+          message
+        )}
       </div>
     </div>
   );
--- src/components/common/chatBubble/types.ts
+++ src/components/common/chatBubble/types.ts
@@ -1,5 +1,12 @@
+import React from 'react';
+
 export type ChatBubbleProps = {
-  message: string;
+  message: string | React.ReactNode;
   isChatbot: boolean;
-  showButton?: boolean;
+  isButtonVisible?: boolean;
+  tarotCard?: {
+    name: string;
+    url: string;
+    direction: '정방향' | '역방향';
+  };
 };
