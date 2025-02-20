43a72a0dbece9e4d058a2847ac46d98e9c62872b
hatnim <dlgotsla3391@gmail.com>
2025-02-20 22:49:18 +0900

    comment: 리뷰 디테일 api 연결 전 주석 처리

--- src/components/reviews/reviewCard/index.tsx
+++ src/components/reviews/reviewCard/index.tsx
@@ -2,54 +2,38 @@ import Image from 'next/image';
 import { Eye } from 'lucide-react';
 
 import useScreenWidth from '@/hooks/useScreenWidth';
-import useLayerCardStore from '@/stores/layerCardStore';
 
-import { layerCard } from '@common/layerCard';
-
+// import useLayerCardStore from '@/stores/layerCardStore';
+// import { layerCard } from '@common/layerCard';
 import TheFool from '@images/TheFool.svg';
 
-import ReviewDetail from '../reviewDetail';
-
+// import ReviewDetail from '../reviewDetail';
 import { ReviewCardProps } from '../types';
 
 const ReviewCard: React.FC<ReviewCardProps> = ({
-  id,
-  chatlog_id,
+  // id,
   title,
   content,
-  nickname,
+  user_nickname,
   created_at,
   updated_at,
   view_count,
 }) => {
-  const { hideLayerCard } = useLayerCardStore();
+  // const { hideLayerCard } = useLayerCardStore();
   const { isCustomWidth } = useScreenWidth(640);
 
   const showLayerCard = () => {
-    layerCard({
-      content: (
-        <ReviewDetail
-          id={id}
-          chatlog_id={chatlog_id}
-          title={title}
-          content={content}
-          nickname={nickname}
-          img_url={TheFool}
-          created_at={created_at}
-          updated_at={updated_at}
-          view_count={view_count}
-          close={() => hideLayerCard()}
-        />
-      ),
-      size: 'max-w-5xl max-h-[768px]',
-    });
+    // layerCard({
+    //   content: <ReviewDetail review_id={id} close={() => hideLayerCard()} />,
+    //   size: 'max-w-5xl max-h-[768px]',
+    // });
   };
 
   return (
-    <li>
+    <li className="w-full">
       <button
         onClick={showLayerCard}
-        className={`flex items-center w-full max-h-32 border border-purple bg-lightPink hover:-translate-y-0.5 hover:shadow-md transition-all duration-200 hover:cursor-pointer ${isCustomWidth ? 'gap-4 px-6 py-4' : 'gap-10 px-6 py-4 '}`}
+        className={`flex items-center w-full max-h-32 border border-purple bg-lightPink hover:-translate-y-0.5 hover:shadow-md transition-all duration-200 hover:cursor-pointer ${isCustomWidth ? 'gap-4 px-4 py-3' : 'gap-5 px-6 py-4 '}`}
         aria-label={`${title} 리뷰 상세보기`}
       >
         <Image
@@ -58,17 +42,21 @@ const ReviewCard: React.FC<ReviewCardProps> = ({
           width={50}
           className={`${isCustomWidth ? 'hidden' : 'block'}`}
         />
-        <div className="flex flex-1 flex-col justify-center h-full text-purple">
-          <div className={`flex items-center ${isCustomWidth ? 'justify-between' : 'gap-5'}`}>
+        <div className="flex flex-col justify-center w-full h-full text-purple">
+          <div
+            className={`flex items-center w-full ${isCustomWidth ? 'justify-between' : 'gap-5'}`}
+          >
             <h2 className={`font-gBold truncate ${isCustomWidth ? 'text-sm' : 'text-base'}`}>
               {title}
             </h2>
-            <p className="text-xs line-clamp-1">{nickname}</p>
+            <p className="text-xs line-clamp-1">{user_nickname}</p>
           </div>
-          <p className={`text-left line-clamp-2 ${isCustomWidth ? 'text-xs' : 'text-sm'}`}>
+          <p
+            className={`w-full min-h-10 text-left line-clamp-2 break-all ${isCustomWidth ? 'text-xs' : 'text-sm'}`}
+          >
             {content}
           </p>
-          <div className="flex justify-between items-center text-xs">
+          <div className="flex justify-between items-center w-full text-xs">
             <time
               dateTime={
                 updated_at || created_at
